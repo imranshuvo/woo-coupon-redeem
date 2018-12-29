7aa9f0539d5b56f23b180d5add3fbecc646407a5
imrankhanshuvo@gmail.com <imrankhanshuvo@gmail.com>
2018-12-29 23:49:40 +0600

    Initial setup

---
+++ woo-coupon-redeem.php
@@ -0,0 +1,12 @@
+<?php if (!defined('ABSPATH')) die('No Direct Access Allowed!');
+/**
+ * Plugin Name: Woo Coupon Redeem
+ * Plugin URI: http://www.ikhan.me
+ * Description: Create a coupon redeem page where a field is used to check for coupons. If the coupon is valid, it is applied and the associated product is added to the cart and the user is redirected to the cart page.
+ * Author: iKhan
+ * Author URI: http://ikhan.me
+ * Version: 1.0.0
+ * Text Domain: woo-coupon-redeem
+ *
+ */
+
